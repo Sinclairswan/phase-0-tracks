26be6204c5df4f3487ef66f4c8d4e4301a92d617
Sinclair Swan <sinclairswan@gmail.com>
2016-11-18 17:33:25 -0600

    decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -17,3 +17,26 @@ while index < encrypt.length
 	end	#if encrypt[index] == " "			
 end
 puts encrypt
+
+
+
+puts "decrypt?"
+decrypt = gets.chomp
+
+new_string = ""
+
+i = 0
+while i < decrypt.length
+	if decrypt[i] != " "
+		str ="abcdefghijklmnopqrstuvwxyz"
+		new_value = str.index(decrypt[i])
+		new_value =new_value -1
+		str[new_value]
+		new_string = new_string + str[new_value]
+		i += 1
+	else
+	  new_string = new_string + " "
+		i += 1
+end
+end
+puts new_string
