a5c8c56bb69a5d3af3f77fab5724a06c4493698f
Sinclair Swan <sinclairswan@gmail.com>
2016-11-28 10:47:41 -0600

    stuck on last release

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,22 +9,22 @@ original_name = false
 until original_name =="quit"
 	
 	puts "what is your name?"
-		original_name = gets.chomp!
-
-		 input_name = original_name.downcase.split.reverse
-
-			input_name.each do |names| 
-		  		names= names.tr_s!('bcdfghjklmnpqrstvwxyz','cdfghjklmnpqrstvwxyb')
-	  				names.tr_s!("aeiou","eioua")
-			end
+	original_name = gets.chomp!
+#lowercase, split words, swap order
+	input_name = original_name.downcase.split.reverse
+#substitute string with next letter
+	input_name.each do |names| 
+		names= names.tr_s!('bcdfghjklmnpqrstvwxyz','cdfghjklmnpqrstvwxyb')
+		names.tr_s!("aeiou","eioua")
+	end
 		
 	 			code_name =	input_name.each do |encrpyt|
 	   				puts "#{encrpyt.capitalize}"
 	   	
 	 			end
-	 				name_info = {
-	 					"Real name" => original_name,
-	 					"Alias" => code_name }
+name_info = {
+	 		"Real name" => original_name,
+	 		"Alias" => code_name }
 
 end
 
