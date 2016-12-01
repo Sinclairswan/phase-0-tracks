544b4520693fb20e08a845f0739c405a1c9b33da
Sinclair Swan <sinclairswan@gmail.com>
2016-11-30 16:31:51 -0800

    add puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+#Species ----------------------
+# Canine domestic
+
+# Charateristics ---------------------
+# Legs: 4
+# Fur color: varies
+# Name: varies
+# Size: varies
+# Adorable: yes
+
+# Behavior -------------------
+# fetch
+# tug
+# roll over
+# play dead
+# laydown
+
+
+class Puppy
+	end
+	
