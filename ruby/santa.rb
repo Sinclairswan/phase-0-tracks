61b4c42c946615a61f7fb7f20b466fa50e1dc097
Sinclair Swan <sinclairswan@gmail.com>
2016-12-03 20:08:32 -0800

    ADD getters and setters

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,13 +2,12 @@
 class Santa
 	def initialize (gender, ethnicity)
 		@gender = "none"
-		@ethnicity = "none"
+		@ethnicity = ethnicity
 		@reindeer_ranking = [
 			"Rudolph", "Dasher", "Dancer", 
 			"Prancer", "Vixen", "Comet", 
 			"Cupid", "Donner", "Blitzen"
 			]
-		reindeer = @reindeer_ranking
 		@age = 0
 		puts "intializing Santa instance..."	
 	end
@@ -16,20 +15,57 @@ class Santa
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
-	def reindeer(reindeer)
-			@reindeer_ranking.each do |deer|
-			puts "On #{deer}"
-			end
-	end
+	# def reindeer
+	# 		@reindeer_ranking.each do |deer|
+	# 		puts "On #{deer}"
+	# 		end
+	#end
 
 	def eat_milk_and_cookies (cookie)
 		puts "That was a good #{cookie} cookie"	
 	end
 
+	def celebrate_birthday
+		@age + 1
 	end
+
+	def get_mad_at(reindeer)
+		#@reindeer_ranking.slice(0)
+		@reindeer_ranking.delete("#{reindeer}")	
+		@reindeer_ranking.insert(-1,"#{reindeer}")
+	end
+	def reindeer
+		@reindeer_ranking 	
+	end
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+
+	def age
+		@age
+	end
+	def ethnicity
+		@ethnicity
+	end
+
+	def about
+		puts "Santa is #{@age} years old, a #{@gender}, gets mad at #{@reindeer_ranking[-1]}"
+	end
+
+end
 
 santas = Santa.new("none","black")
 santas.speak
 santas.eat_milk_and_cookies("peanut butter")
-santas.reindeer("bob")
+#santas.reindeer
+santas.get_mad_at("Vixen")
+puts santas.reindeer
+puts santas.age 
+puts santas.celebrate_birthday
+puts santas.gender = "female"
+puts santas.ethnicity
+puts santas.about
+
+
 
