801052f7028a9e4d2870c0cc1c9e7adeee7609f0
Sinclair Swan <sinclairswan@gmail.com>
2017-01-16 08:57:41 -0600

    add release 0-2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+#Release 0
+
 arr = [1,2,3,4,5,6,3,5,4,4,224,43,435,5,45,24,52,5,235,20]
 
 
@@ -13,8 +15,8 @@ end
 
 puts search(arr,0)
 
-
-
+#-------------------------------------------
+#Release 1
 
 def fib (digits)
 	fib_array = [0,1]
@@ -37,3 +39,37 @@ if fib(100) == 218922995834555169026
 else
 	print "its not alive"
 end
+
+#------------------------------------------------
+#Release 2
+ #Sorting methods - Bubble sort compares each set of values and the switches thier 
+ # postion based on which values is higher and will continue through each pair until 
+ # there are no more values to compare.
+
+ #Pseudocode 
+ #Compare values in an array 2 at a time.
+ #if value at [0] is > than value at[1] then move value at [0] to value at [1]
+ # Repeat process until no values can be moved
+ #return a new array
+
+def bubble_sort(n)
+  return n if n.length <= 1
+
+  0.upto(n.length - 1) do |t|
+    0.upto(n.length - 2 - t) do |i|
+      if n[i] > n[i + 1]
+        n[i], n[i + 1] = n[i + 1], n[i]
+      end
+    end
+  end
+  n
+end
+
+test_arr =[5,3,2,4,6,7,2,1,9]
+
+bubble_sort(test_arr)
+
+
+
+
+
