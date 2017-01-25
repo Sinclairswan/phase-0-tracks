7d8f9197239a489de77f6d939c6e30c18a287a85
Sinclair Swan <sinclairswan@gmail.com>
2017-01-24 18:51:31 -0600

    finished all releases

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,18 @@ for (var i = 0; i < colors.length; i++) {
 	  }
 
 console.log(horseList)
+
+function Car(make, model, year) {
+
+	console.log("Brand new car:", this);
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.go = function() {console.log("vroom");};
+
+	console.log("Your car is ready!");
+}	
+
+var firstCar = new Car("honda", "civic", 2017);
+console.log(firstCar);
+firstCar.go();
