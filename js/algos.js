f059cbecddd278a84a8f2981af6c81ea6a5abe98
Sinclair Swan <sinclairswan@gmail.com>
2017-01-29 11:58:09 -0600

    add randomString function

--- js/algos.js
+++ js/algos.js
@@ -3,7 +3,7 @@ var array1 = ["long phrase","longest phrase","longer phrase"]
 
 function findBig (array){
   var largest = 0;
-var longestWord = " ";
+  var longestWord = " ";
 
 	for (var i = 0; i < array.length; i++) {
 	    if (array[i].length > largest) {
@@ -27,9 +27,35 @@ function obCompare ({name: x, age: y}, {name: a, age: b}){
 
 
 
-// Driver code
+function randomString(num) {  
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+    var result = []
+    var words = []
+for (var x = 0; x < num; x++){ // replace 10 with num
+    for (var i = 0; i< Math.floor(Math.random() * 12); i++) {
+    result.push(possible[Math.floor(Math.random() * 27)])
+    }
+ // joins the array into strings
+var word = result.join('');
 
-console.log(findBig(array1))
+words.push (word);
+ 
+}
+
+return words;
+}
+
+
+
+
+
+
+
+// Driver code
+ 
+console.log(randomString(5));
+console.log(findBig( (randomString(5))));
+console.log(findBig(array1));
 console.log(obCompare({name: "Steven", age: 52},{name: "Steven", age: 54}));
 
 
