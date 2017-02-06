e11da0119cf2a456a16a8a05a4d9a5d92c343165
Sinclair Swan <sinclairswan@gmail.com>
2017-02-06 16:33:12 -0600

    add movies print out

--- databases/awesome_project/horror_movies.rb
+++ databases/awesome_project/horror_movies.rb
@@ -3,7 +3,7 @@ require 'sqlite3'
 
 
 db = SQLite3::Database.new("horror.db")
-
+db.results_as_hash = true
 create_movie_table = <<-SQL
   CREATE TABLE IF NOT EXISTS movies(
     id INTEGER PRIMARY KEY,
@@ -26,10 +26,16 @@ create_movie_table = <<-SQL
 
  db.execute(create_movie_table)
  db.execute(create_platforms_table)
+
  movies = db.execute("SELECT * FROM movies")
+ puts movies.class
+ movies.each do |movie|
+ 	puts "#{movie['title']} was directed by #{movie['director']} and was released in #{movie['release_date']}"
+ end
+
  #test data
  #db.execute(<<-SQL  
  #	INSERT INTO movies (title, director, release_date, trailer_link, platform_id) 
  #	VALUES ("Aliens", "Ridely Scott", 1986, "www.youtube.com/watch?v=XKSQmYUaIyE", 2);
  #SQL
- )
+ #)
